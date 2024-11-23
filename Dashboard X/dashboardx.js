d17cab924dc8897788283ba93b0c87b65883b202
Banger dev <bestordinioha@gmail.com>
2024-11-23 14:44:50 +0100

    pro
    promfmffm

--- Dashboard X/dashboardx.js
+++ Dashboard X/dashboardx.js
@@ -48,3 +48,32 @@ document.querySelectorAll('.to-be-selected').forEach(element => {
         element.classList.add('selected');
     });
 });
+
+
+
+
+
+
+const container = document.querySelector('.earnings-slider');
+let startX;
+
+container.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+});
+
+container.addEventListener('touchend', (e) => {
+  const endX = e.changedTouches[0].clientX;
+  const diffX = startX - endX;
+
+  if (Math.abs(diffX) > 50) {
+    // Swipe left
+    if (diffX > 0) {
+      container.scrollBy({ left: window.innerWidth, behavior: 'smooth' });
+    }
+    // Swipe right
+    else {
+      container.scrollBy({ left: -window.innerWidth, behavior: 'smooth' });
+    }
+  }
+});
+
